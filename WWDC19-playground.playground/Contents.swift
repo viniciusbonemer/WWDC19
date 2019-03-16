1e45ccae098fb226d34833cc5de27d3967df22da
viniciusbonemer <v.bonemer@gmail.com>
2019-03-16 09:06:20 -0300

    Add playground to project

---
+++ WWDC19-playground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()

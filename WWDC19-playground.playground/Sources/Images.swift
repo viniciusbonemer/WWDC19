315fa4d686788427fa2ee10c6f7b2838a9c8d44f
viniciusbonemer <v.bonemer@gmail.com>
2019-03-16 09:33:43 -0300

    Add Images.swift

---
+++ WWDC19-playground.playground/Sources/Images.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+public
+enum Images {
+    
+    public static
+    var arrowUp: UIImage {
+        return UIImage(named: "ArrowUp")!
+    }
+    
+    public static
+    var arrowDown: UIImage {
+        return UIImage(named: "ArrowDown")!
+    }
+    
+    public static
+    var airplaneGlyph: UIImage {
+        return UIImage(named: "AirplaneGlyph")!
+    }
+    
+    public static
+    var locationGlyph: UIImage {
+        return UIImage(named: "LocationGlyph")!
+    }
+    
+    public static
+    var calendarGlyph: UIImage {
+        return UIImage(named: "CalendarGlyph")!
+    }
+    
+}

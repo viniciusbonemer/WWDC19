bc399c3185d08f61e30889be05af5f5854906863
viniciusbonemer <v.bonemer@gmail.com>
2019-03-16 09:18:16 -0300

    Add Colors.swift file

---
+++ WWDC19-playground.playground/Sources/Colors.swift
@@ -0,0 +1,45 @@
+//
+//  Colors.swift
+//  WWDC19
+//
+//  Created by Vinícius Bonemer on 16/03/19.
+//  Copyright © 2019 Vinícius Bonemer. All rights reserved.
+//
+
+import UIKit
+
+public
+enum Colors {
+    
+    public static
+    var darkGray: UIColor {
+        return UIColor(red: 118/225, green: 118/225, blue: 118/225, alpha: 1)
+    }
+    
+    public static
+    var gray: UIColor {
+        return UIColor(red: 158/225, green: 161/225, blue: 165/225, alpha: 1)
+    }
+    
+    public static
+    var lightGray: UIColor {
+        return UIColor(red: 185/225, green: 188/225, blue: 185/225, alpha: 1)
+    }
+    
+    public static
+    var blue: UIColor {
+        return UIColor(red: 68/225, green: 53/225, blue: 217/225, alpha: 1)
+    }
+    
+    public static
+    var pink: UIColor {
+        return UIColor(red: 206/225, green: 159/225, blue: 252/225, alpha: 1)
+    }
+    
+    public static
+    var purple: UIColor {
+        return UIColor(red: 115/225, green: 103/225, blue: 240/225, alpha: 1)
+    }
+    
+}
+
